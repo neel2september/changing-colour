080a4ab685f8b59b72984f420d318c07e20030b8
neel2september <105707712+neel2september@users.noreply.github.com>
2022-05-18 15:45:53 +0530

    Add files via upload

---
+++ Pro-c9-sa-reference-main/var bov.js
@@ -0,0 +1,15 @@
+var box;
+var a
+var b
+a = 1
+b = 2-1
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+
+}
+function draw() 
+{
+   background("yellow");
+}if (a = b)
+background("red")
